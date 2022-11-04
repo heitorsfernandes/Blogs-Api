8de050429f37a666fc1e5bc51088f067ee8a3097
HeitorFernandes <heitorsoaresfer@gmail.com>
2022-11-04 08:18:22 -0300

    Requisito 10

--- src/models/BlogPost.js
+++ src/models/BlogPost.js
@@ -1,29 +1,30 @@
 const { DataTypes, Model } = require('sequelize');
 
 module.exports = (sequelize) => {
-    const User = sequelize.define('User', {
+    const BlogPost = sequelize.define('BlogPost', {
         id: { 
           autoIncrement:true,
           allowNull:false,
           primaryKey:true,
           type: DataTypes.INTEGER 
         },
-        displayName: DataTypes.STRING,
-        email: DataTypes.STRING,
-        password: DataTypes.STRING,
-        image: DataTypes.STRING,
+        title: DataTypes.STRING,
+        content: DataTypes.STRING,
+        userId: DataTypes.INTEGER,
+        published: DataTypes.STRING,
+        updated: DataTypes.STRING,
     }, {
         timestamps: false,
         tableName: 'users',
         underscored: true,
     })
 
-    User.associate = (models) => {
-        User.hasMany(models.BlogPost, {
+    BlogPost.associate = (models) => {
+        BlogPost.belongsTo(models.User, {
             foreignKey: 'userId',
-            as: 'blog_post'
+            as: 'user'
         })
     };
 
-    return User;
+    return BlogPost;
 }
